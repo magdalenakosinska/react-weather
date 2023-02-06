31e4997d947134944e4840723b3d305e1965a072
magdalenakosinska <59508924+magdalenakosinska@users.noreply.github.com>
2023-02-06 14:18:18 +0100

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello Weather</div>;
+  return (
+    <div className="App">
+      <div className="container">Hello Weather</div>
+    </div>
+  );
 }
