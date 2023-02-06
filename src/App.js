eaa54e44954449144ae9fd3bd75febbc780b5414
magdalenakosinska <59508924+magdalenakosinska@users.noreply.github.com>
2023-02-06 15:08:40 +0100

    created html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">Hello Weather</div>
+      <Weather />
     </div>
   );
 }
